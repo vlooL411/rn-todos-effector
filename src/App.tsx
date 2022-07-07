3c3ffdede3c01d1af7d1c7dfec1dfdf57d3e2f72
vlooL <52248468+vlooL411@users.noreply.github.com>
2022-07-07 19:43:26 +0300

    Исправил цвет фона для тёмной темы.

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ const App = () => {
   );
 };
 
-const styles = StyleSheet.create({container: {flex: 1}});
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: '#fff'},
+});
 
 export default App;
