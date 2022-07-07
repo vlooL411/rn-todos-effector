012f8c5605abd2fd9d5c8c98923f0af61adaf869
vlooL <52248468+vlooL411@users.noreply.github.com>
2022-07-07 11:04:24 +0300

    Добавил Todos

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,14 +1,60 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {useStore} from 'effector-react';
+import React, {useCallback, useEffect} from 'react';
+import {
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+} from 'react-native';
+import Todo, {AddTodo} from '../Todo';
+import Categories from '../Categories';
+import TodosHeader from './Header';
+import {
+  $todosCategories,
+  todosCategoryAdd,
+  todosCategoryChange,
+  todosCategoryRemove,
+} from './store';
+import {$visibleTodos, todosAdd} from './store/todos';
 
 const Todos = () => {
+  const todosCategories = useStore($todosCategories);
+  const visibleTodos = useStore($visibleTodos);
+
+  const onAddTodo = useCallback(() => {
+    todosAdd({
+      id: `${Math.random()}`,
+      title: '',
+      categories: todosCategories.filter(c => c),
+      completed: false,
+    });
+  }, [todosCategories]);
+
   return (
-    <View>
-      <Text>Todos</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <TodosHeader />
+      <Categories
+        categories={todosCategories}
+        onChange={(index, category) => todosCategoryChange({index, category})}
+        onAdd={todosCategoryAdd}
+        onRemove={todosCategoryRemove}
+      />
+      <FlatList
+        data={visibleTodos}
+        keyExtractor={item => item.id}
+        renderItem={({item, index}) => {
+          return <Todo {...item} index={index} onPress={() => {}} />;
+        }}
+        ListHeaderComponent={<AddTodo onAdd={onAddTodo} />}
+      />
+    </KeyboardAvoidingView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {backgroundColor: '#fff', flex: 1, height: '100%'},
+});
 
 export default Todos;
