53897d8fe817df8607caa3bf6bd7fc9547bd05f7
vlooL <52248468+vlooL411@users.noreply.github.com>
2022-07-15 12:31:09 +0300

    Добавил eslint-plugin-effector

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['effector', '@typescript-eslint'],
+  extends: [
+    '@react-native-community',
+    'plugin:effector/recommended',
+    'plugin:effector/scope',
+  ],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
